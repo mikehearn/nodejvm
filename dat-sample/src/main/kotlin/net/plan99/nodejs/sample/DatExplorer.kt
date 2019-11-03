548498997398e7e4ce4c43813e46e942fa274719
Mike Hearn <mike@r3.com>
2019-11-03 01:30:44 +0100

    Update the DAT sample.

--- dat-sample/src/main/kotlin/net/plan99/nodejs/sample/DatExplorer.kt
+++ dat-sample/src/main/kotlin/net/plan99/nodejs/sample/DatExplorer.kt
@@ -3,18 +3,21 @@
 package net.plan99.nodejs.sample
 
 import net.plan99.nodejs.kotlin.nodejs
+import org.graalvm.polyglot.Value
 import java.io.File
 import java.util.function.Consumer
 
-interface MemoryUsage {
-    fun rss(): Long
-    fun heapTotal(): Long
+interface DatConnection {
+    val host: String
+    val port: Int
+    val type: String
 }
 
-interface DatConnection {
-    fun host(): String
-    fun port(): Int
-    fun type(): String
+object ConnectionHandler : Consumer<Value> {
+    override fun accept(v: Value) = nodejs {
+        val conn = v.cast<DatConnection>()
+        println("New connection to ${conn.host}:${conn.port} using ${conn.type}")
+    }
 }
 
 fun main() {
@@ -22,17 +25,7 @@ fun main() {
         val downloadPath by bind(File("download"))
         if (downloadPath.exists()) downloadPath.deleteRecursively()
 
-        val r: MemoryUsage = eval("process.memoryUsage()")
-        println("rss is ${r.rss()}, heapTotal is ${r.heapTotal()}")
-
-        val callback by bind(Consumer { m: Map<String, Any?> ->
-            val map = m.asValue().cast<DatConnection>()
-            val host: String = map.host()
-            val port: Int = map.port()
-            val type: String = map.type()
-            println("""New connection to $host:$port using $type""")
-        })
-
+        val callback by bind(ConnectionHandler)
         run("""
             let Dat = require('dat-node');
             Dat(downloadPath.getName(), { key: "778f8d955175c92e4ced5e4f5563f69bfec0c86cc6f670352c457943666fe639" }, function(err, dat) {
@@ -46,5 +39,5 @@ fun main() {
          """)
     }
 
-    Thread.sleep(Long.MAX_VALUE)
+    Thread.sleep(5000)
 }
